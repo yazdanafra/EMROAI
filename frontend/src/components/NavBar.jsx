3d508b377552d2964e024a6905c076450a7fb958
yazdanafra <yazdan9093@gmail.com>
2025-12-20 22:56:42 +0330

    pages, components

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const NavBar = () => {
+  return (
+    <div>
+      <img src={assets.logo} />
+    </div>
+  );
+};
+
+export default NavBar;
