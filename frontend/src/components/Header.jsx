d0ecba11ceda6774daee526afbc44dd374e380d5
yazdanafra <yazdan9093@gmail.com>
2025-12-24 19:05:59 +0330

    logo, navbar, header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      {/* .............left side............. */}
+      <div></div>
+      {/* .............right side............. */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
+
   return (
-    <div>
-      <img src={assets.logo} />
+    <div className="flex items-center justify-between text-sm py-4 border-b border-b-gray-400">
+      <img className=" w-44 cursor-pointer" src={assets.logo} />
+      <ul className="hidden md:flex items-start gap-5 font-medium">
+        <NavLink to="/">
+          <li className="py-1">HOME</li>
+          <hr className="border-none outline-none h-0.5 m-auto hidden w-3.5 bg-primary" />
+        </NavLink>
+
+        <NavLink to="/doctors">
+          <li className="py-1">ALL DOCTORS</li>
+          <hr className="border-none outline-none h-0.5 m-auto hidden w-3.5 bg-primary" />
+        </NavLink>
+
+        <NavLink to="/about">
+          <li className="py-1">ABOUT</li>
+          <hr className="border-none outline-none h-0.5 m-auto hidden w-3.5 bg-primary" />
+        </NavLink>
+
+        <NavLink to="/contact">
+          <li className="py-1">CONTACT</li>
+          <hr className="border-none outline-none h-0.5 m-auto hidden w-3.5 bg-primary" />
+        </NavLink>
+      </ul>
+
+      <div className="flex items-center gap-4">
+        {token ? (
+          <div className="flex items-center gap-2 cursor-pointer group relative">
+            <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
+            <img className="w-2.5" src={assets.dropdown_icon} alt="" />
+            <div className="absolute right-0 top-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+              <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                <p
+                  onClick={() => navigate("my-profile")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My Profile
+                </p>
+                <p
+                  onClick={() => navigate("my-appointments")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My Appointments
+                </p>
+                <p
+                  onClick={() => setToken(false)}
+                  className="hover:text-black cursor-pointer"
+                >
+                  Logout
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <button
+            onClick={() => navigate("/login")}
+            className="cursor-pointer bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block"
+          >
+            Create Account
+          </button>
+        )}
+      </div>
     </div>
   );
 };
