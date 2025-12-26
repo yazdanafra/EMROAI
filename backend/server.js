b27ed4d3a741eceeeec379f67a8c11bb318d202f
yazdanafra <yazdan9093@gmail.com>
2025-12-26 19:41:17 +0330

    backend init

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
+app.use("/api/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => console.log("Server started", port));
